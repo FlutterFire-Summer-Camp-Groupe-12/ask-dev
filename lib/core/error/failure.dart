c23b120c6123461129662df97ab1b57495b2b25c
ElanaStacy <elanastacyi@gmail.com>
2026-09-10 07:38:57 +0200

    Ajout des classes d erreur

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -8,3 +8,12 @@ class Failure extends Equatable {
   @override
   List<Object?> get props => [message];
 }
+class ServerFailure extends Failure {
+  const ServerFailure({required super.message});
+}
+class CacheFailure extends Failure {
+  const CacheFailure({required super.message});
+}
+class NetworkFailure extends Failure {
+  const NetworkFailure({required super.message});
+}
